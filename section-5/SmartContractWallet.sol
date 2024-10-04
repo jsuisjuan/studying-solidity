3ece8ebd9491febec0761ede63a08ea4dbd2bc93
jsuisjuan <juanc.s.delima@gmail.com>
2024-10-03 22:03:13 -0300

    adding smartcontractwallet

---
+++ section-5/SmartContractWallet.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
+contract Consumer {
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+    function deposit()public payable {}
+}
+contract SmartContractWallet {
+    address payable public owner;
+    address payable nextOwner;
+    uint guardiansResetCount;
+    uint public constant confirmationsFromGuardiansForReset = 3;
+    mapping(address => uint) public allowance;
+    mapping(address => bool) public isAllowedToSend;
+    mapping(address => bool) public guardians;
+    mapping(address => mapping(address => bool)) nextOwnerGuardianVotedBool;
+    constructor() {
+        owner = payable(msg.sender);
+    }
+    receive() external payable {}
+    function setGuardian(address _guardian, bool _isGuardian) public {
+        require(msg.sender == owner, "You are not the owner, aborting");
+        guardians[_guardian] = _isGuardian;
+    }
+    function proposeNewOwner(address payable _newOwner) public {
+        require(guardians[msg.sender], "You are not guardian of this wallet, aborting");
+        require(nextOwnerGuardianVotedBool[_newOwner][msg.sender] == false, "You already voted, aborting");
+        if(_newOwner != nextOwner) {
+            nextOwner = _newOwner;
+            guardiansResetCount = 0;
+        }
+        guardiansResetCount++;
+        if(guardiansResetCount >= confirmationsFromGuardiansForReset) {
+            owner = nextOwner;
+            nextOwner = payable(address(0));
+        }
+    }
+    function setAllowance(address _for, uint _amount) public {
+        require(msg.sender == owner, "You are not the owner, aborting");
+        allowance[_for] = _amount;
+        isAllowedToSend[_for] = _amount > 0;
+    }
+    function transfer(address payable _to, uint _amount, bytes memory _payload) public returns(bytes memory) {
+        if(msg.sender != owner) {
+            require(isAllowedToSend[msg.sender], "You are not allowed to send anything from this smart contract, aborting");
+            require(allowance[msg.sender] >= _amount, "You are trying to send more than you are allowed to, aborting");
+            allowance[msg.sender] -= _amount;
+        }
+        (bool success, bytes memory returnData) = _to.call{value:_amount}(_payload);
+        require(success, "Aborting, call was not successful");
+        return returnData;
+    }
+}
